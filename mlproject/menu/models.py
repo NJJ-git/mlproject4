c8897f961353ba333f0c7b463cc19414e93eadae
NJ <gorun@naver.com>
2022-10-22 09:31:12 +0900

    카테고리 추가, 메뉴페이지 리스트업

--- mlproject/menu/models.py
+++ mlproject/menu/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    cateNo = models.IntegerField(default=0, primary_key=True)
+    cName = models.CharField(max_length=20)
+
+
+class Menu(models.Model):
+    productNo = models.IntegerField(default=0, primary_key=True)  # 기본키 설정
+    mName = models.CharField(max_length=20)
+    mimg_path = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)
+    explain = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
--- mlproject/menu/views.py
+++ mlproject/menu/views.py
@@ -1,13 +1,22 @@
 from http.client import HTTPResponse
+from unicodedata import category
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.contrib import auth 
+from django.contrib import auth
 from faceDetect.forms import *
+from .models import Menu
 
 
 # Create your views here.
 def main(request):
-    return render(request, 'menu/main.html')
-def logout(request) :    
-    request.session.flush() #로그아웃
+    menulist = Menu.objects.all()
+    tealist = Menu.objects.filter(category_id=30)
+    beveragelist = Menu.objects.filter(category_id=20)
+    coffeelist = Menu.objects.filter(category_id=10)
+
+    return render(request, 'menu/main.html', {'menulist': menulist, 'tealist': tealist, 'coffeelist': coffeelist, 'beveragelist': beveragelist})
+
+
+def logout(request):
+    request.session.flush()  # 로그아웃
     return HttpResponseRedirect("/faceDetect/")
